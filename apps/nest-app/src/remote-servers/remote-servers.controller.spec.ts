3a094280bb3ceea8125c379b3d65d5dbd7943c30
Jabez <boycalledjabez@gmail.com>
2026-09-25 18:08:49 +0530

    test: Added remote tests and custom cases and solved the tests

--- apps/nest-app/src/remote-servers/remote-servers.controller.spec.ts
+++ apps/nest-app/src/remote-servers/remote-servers.controller.spec.ts
@@ -4,17 +4,25 @@ import { RemoteServersService } from './remote-servers.service.js';
 
 describe('RemoteServersController', () => {
   let controller: RemoteServersController;
+  let service: Mocked<RemoteServersService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [RemoteServersController],
-      providers: [RemoteServersService],
+      providers: [
+        {
+          provide: RemoteServersService,
+          useValue: mock<RemoteServersService>(),
+        },
+      ],
     }).compile();
 
     controller = module.get<RemoteServersController>(RemoteServersController);
+    service = module.get<Mocked<RemoteServersService>>(RemoteServersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+    expect(service).toBeDefined();
   });
 });
--- apps/nest-app/src/remote-servers/remote-servers.service.spec.ts
+++ apps/nest-app/src/remote-servers/remote-servers.service.spec.ts
@@ -3,6 +3,7 @@ import { RemoteServersService } from './remote-servers.service.js';
 import { RemoteServer, RemoteServerStatus } from './entities/remote-server.entity.js';
 import { Repository } from 'typeorm/browser';
 import { getRepositoryToken } from '@nestjs/typeorm';
+import { NotFoundException } from '@nestjs/common';
 
 describe('RemoteServersService', () => {
   let service: RemoteServersService;
@@ -50,4 +51,72 @@ describe('RemoteServersService', () => {
 
     })
   })
+
+  describe('find all', () => {
+    it('it should return all users' , async () => {
+      const remoteServers = [] as any;
+      repo.find.mockResolvedValue(remoteServers);
+
+      const result = await service.findAll();
+      expect(repo.find).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(remoteServers);
+    })
+  })
+
+  describe('find one', () => {
+    it('should find one remote server', async () => {
+      const remoteServer = {} as any;
+      repo.findOne.mockResolvedValue(remoteServer);
+
+      const result = await service.findOne('1', '1');
+      expect(repo.findOne).toHaveBeenCalledTimes(1);
+      expect(repo.findOne).toHaveBeenCalledWith({where: {id: '1', ownerId: '1'}});
+      expect(result).toEqual(remoteServer);
+    })
+  })
+
+  describe('update', () => {
+    it('should update a remote server', async () => {
+      const remoteServer = {} as any;
+      repo.findOne.mockResolvedValue(remoteServer);
+      repo.save.mockResolvedValue(remoteServer);
+      repo.merge.mockReturnValue(remoteServer);
+
+      const result = await service.update('1', {} as any, '1');
+      expect(repo.findOne).toHaveBeenCalledTimes(1);
+      expect(repo.findOne).toHaveBeenCalledWith({where: {id: '1', ownerId: '1'}});
+      expect(repo.merge).toHaveBeenCalledTimes(1);
+      expect(repo.merge).toHaveBeenCalledWith(remoteServer, {} as any);
+      expect(repo.save).toHaveBeenCalledTimes(1);
+      expect(repo.save).toHaveBeenCalledWith(remoteServer);
+      expect(result).toEqual(remoteServer);
+    })
+
+    it('should return not found when remote server is not found', async () => {
+      repo.findOne.mockResolvedValue(null);
+
+      await expect(service.update('1', {} as any, '1')).rejects.toThrow(NotFoundException);
+    })
+  })
+
+  /* 
+  describe('remove', () => {
+    it('should remove a server', async () => {
+      repo.delete.mockResolvedValue({affected: 1});
+
+      const result = await service.remove('1', '1');
+      expect(repo.delete).toHaveBeenCalledTimes(1);
+      expect(repo.delete).toHaveBeenCalledWith({id: '1', ownerId: '1'});
+      expect(result).toEqual(undefined);
+    })
+
+    it('should return not found when remote server is not found', async () => {
+      repo.delete.mockResolvedValue({affected: 0});
+
+      await expect(service.remove('1', '1')).rejects.toThrow(NotFoundException);
+    })
+  })
+  */
+
+
 });
